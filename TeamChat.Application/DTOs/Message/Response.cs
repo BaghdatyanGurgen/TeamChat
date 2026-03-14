5e9096d82c2662dc57aa9879e4b7e1e142fc5f06
gurgen.baghdatyan <gurgen.baghdatyan@teamsolutions.am>
2026-03-14 21:50:42 +0400

    Add avatar in message

--- TeamChat.Application/DTOs/Message/Response.cs
+++ TeamChat.Application/DTOs/Message/Response.cs
@@ -1,7 +1,7 @@
 ﻿namespace TeamChat.Application.DTOs.Message;
 
-public record MessageResponse(Guid Id, Guid SenderId, string SenderName, string Content, DateTime CreatedAt)
+public record MessageResponse(Guid Id, Guid SenderId, string SenderName, string? SenderAvatarUrl, string Content, DateTime CreatedAt)
 {
     public MessageResponse(Domain.Entities.Message m)
-        : this(m.Id, m.SenderId, $"{m.Sender.FirstName} {m.Sender.LastName}".Trim(), m.Content, m.SentAt) { }
+        : this(m.Id, m.SenderId, $"{m.Sender.FirstName} {m.Sender.LastName}".Trim(), m.Sender.AvatarUrl, m.Content, m.SentAt) { }
 }
